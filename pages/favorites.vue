6441479cbe36b3d8890452105a4c20eb5e8aeb25
Franz Bendezu <franzant20@gmail.com>
2021-08-21 14:02:23 -0500

    add star animation

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -13,15 +13,38 @@
     <template #top-items-left="{item}">
       <schedule-favorite-add v-if="item" :favorites-schedules.sync="schedules" :schedule="item" />
     </template>
+    <template #emptyBody>
+      <div>
+        <div id="bm" />
+        <div>Agrega tus horario favoritos</div>
+      </div>
+    </template>
   </schedules-presentation>
 </template>
 
 <script lang="ts">
+import Lottie from 'lottie-web'
 import { Component, Vue } from 'nuxt-property-decorator'
+import SchedulesPresentation from '~/components/SchedulesPresentation.vue'
 @Component({
-  layout: 'app'
+  layout: 'app',
+  components: {
+    SchedulesPresentation
+
+  }
 })
+
 export default class Favorites extends Vue {
+  mounted () {
+    const animation = Lottie.loadAnimation({
+      container: document.getElementById('bm') as Element,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('~/assets/lottie/156-star-blast.json')
+    })
+  }
+
   get schedules () {
     return this.$store.state.modules.MyData.myFavoritesSchedules
   }
