b0251740320c2d319635425371cce0598b877fd1
Franz Bendezu <franzant20@gmail.com>
2021-08-24 16:46:03 -0500

    config tittle

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - horext-ssr-client',
-    title: 'horext-ssr-client',
+    titleTemplate: '%s - Horext',
+    title: 'Bienvenido',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
