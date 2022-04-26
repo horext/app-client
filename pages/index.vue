6aed7ab1ff4e94c6b8cf27397fafa5b3088cc27b
Franz Bendezu <franzant20@gmail.com>
2022-04-26 10:02:11 -0500

    feature/list-feats

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,88 @@
 <template>
-  <v-row no-gutters>
+  <div>
     <v-img
       :src="require('~/assets/material2.jpg')"
+      max-height="350"
       gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
     >
       <v-container fill-height>
-        <v-row
-          align="center"
-          class="white--text mx-auto"
-          justify="center"
-        >
-          <v-col
-            class="white--text text-center"
-            cols="12"
-            tag="h1"
-          >
-            <span
-              :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
-              class="font-weight-light"
-            >
-              Bienvenido a
-            </span>
+        <v-row align="center" class="white--text mx-auto" justify="center">
+          <v-col class="white--text text-center" cols="12" tag="h1">
             <br>
-            <span
-              :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
-              class="font-weight-black"
-            >
+            <span class="font-weight-black display-3 text-sm-display-4">
               Horext
             </span>
           </v-col>
 
-          <v-btn
-            class="align-self-end"
-            outlined
-            dark
-            to="/generator"
-          >
+          <v-btn class="align-self-end" outlined dark to="/generator">
             <v-icon>mdi-chevron-double-right</v-icon>
             Generar mis horarios
           </v-btn>
         </v-row>
       </v-container>
     </v-img>
-  </v-row>
+    <v-container>
+      <v-row
+        no-gutters
+        align="center"
+        justify="center"
+        class="fill-height mx-auto"
+      >
+        <v-col cols="12">
+          <h2 class="text-center">
+            Funcionalidades
+          </h2>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-card  class="text-center"  dark color="success" outlined>
+            <v-card-title>
+              <v-icon class="mx-auto text-h3">
+                mdi-calendar-clock
+              </v-icon>
+            </v-card-title>
+            <v-card-title class="text-center">
+              Generar tus Horarios
+            </v-card-title>
+            <v-card-text>
+              <p>Selecciona tus cursos y genera todos tus horarios posibles</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-card height="200" outlined>
+            <v-card-title>
+              <h3 class="headline mb-0">
+                <v-icon>mdi-account-multiple</v-icon>
+                Usuarios
+              </h3>
+            </v-card-title>
+            <v-card-text>
+              <p>Administrar usuarios y roles.</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-card height="200" outlined>
+            <v-card-title>
+              <h3 class="headline mb-0">
+                <v-icon>mdi-account-multiple-plus</v-icon>
+                Crear usuarios
+              </h3>
+            </v-card-title>
+            <v-card-text>
+              <p>Crear usuarios para administrar el sistema.</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {
-  }
+  components: {}
 }
 </script>
 <style>
-
 </style>
