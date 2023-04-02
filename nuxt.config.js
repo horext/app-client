4bbf551d8f0ba60f2af0f46a9f62c3792a784973
Franz Bendezu <franzant20@gmail.com>
2023-04-02 12:01:25 -0500

    fix: remove nuxt pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    // '@nuxtjs/pwa',
     '@nuxtjs/universal-storage'
   ],
 
