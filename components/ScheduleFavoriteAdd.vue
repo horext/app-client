9023778c45165981b14960d2673048fd8fa461ea
Franz Bendezu <franzant20@gmail.com>
2023-08-16 12:11:19 -0500

    refactor: migrate to useVModel

--- components/ScheduleFavoriteAdd.vue
+++ components/ScheduleFavoriteAdd.vue
@@ -4,10 +4,10 @@
       fab
       outlined
       icon
-      :color="isFavorite?'yellow':null"
+      :color="isFavorite ? 'yellow' : null"
       @click="changeFavoriteState"
     >
-      <v-icon :color="isFavorite?'yellow':null">
+      <v-icon :color="isFavorite ? 'yellow' : null">
         mdi-star
       </v-icon>
     </v-btn>
@@ -16,6 +16,7 @@
 
 <script lang="ts">
 import { computed, defineComponent, PropType, ref } from 'vue'
+import { useVModel } from '@vueuse/core'
 import { useSnackbar } from '~/composables/snackbar'
 
 export default defineComponent({
@@ -30,24 +31,11 @@ export default defineComponent({
       default: () => []
     }
   },
+  emits: ['update:schedule', 'update:favoritesSchedules'],
   setup (props, { emit }) {
-    const currentSchedule = computed<any>({
-      get () {
-        return props.schedule
-      },
-      set (schedule) {
-        emit('update:schedule', schedule)
-      }
-    })
+    const currentSchedule = useVModel(props, 'schedule', emit)
 
-    const favoritesSchedulesSync = computed< any[]>({
-      get () {
-        return props.favoritesSchedules
-      },
-      set (favoritesSchedules) {
-        emit('update:favoritesSchedules', favoritesSchedules)
-      }
-    })
+    const favoritesSchedulesSync = useVModel(props, 'favoritesSchedules', emit)
 
     const showMessage = ref(false)
     const message = ref('')
@@ -59,10 +47,15 @@ export default defineComponent({
           schedules.splice(indexSchedule.value, 1)
           favoritesSchedulesSync.value = schedules
         } else {
-          favoritesSchedulesSync.value = [...favoritesSchedulesSync.value, props.schedule]
+          favoritesSchedulesSync.value = [
+            ...favoritesSchedulesSync.value,
+            props.schedule
+          ]
         }
         snackbar.showMessage({
-          content: !isFavorite.value ? 'Agregado a favoritos' : 'Quitado de Favoritos',
+          content: !isFavorite.value
+            ? 'Agregado a favoritos'
+            : 'Quitado de Favoritos',
           timeout: 2000,
           color: 'yellow darken-3'
         })
@@ -94,6 +87,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
