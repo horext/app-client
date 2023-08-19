4f859423d5c663797e1235faeb98e836434df63a
Franz Bendezu <franzant20@gmail.com>
2023-08-19 13:32:24 -0500

    fix: remove nuxt components autoimport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,9 +33,6 @@ const config: NuxtConfig = {
     '~/plugins/api-accessor'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
