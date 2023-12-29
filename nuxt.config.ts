5f853cc92e8c53c3d71767d82022e2dd46cd0ab2
Franz Bendezu <franzant20@gmail.com>
2023-12-29 10:35:41 -0500

    refactoe: setup app head in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,24 @@ import vuetify from 'vite-plugin-vuetify'
 
 const config = defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - Horext',
-    title: 'Bienvenido',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          'Horext es una aplicación web que te ayuda a generar horarios ',
-      },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  app: {
+    head: {
+      titleTemplate: '%s - Horext',
+      title: 'Bienvenido',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Horext es una aplicación web que te ayuda a generar horarios ',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'vuetify/lib/styles/main.sass',
