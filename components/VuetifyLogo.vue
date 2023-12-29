cdd33a2c61e43952ac0fbb8f7b31773f69240565
Franz Bendezu <franzant20@gmail.com>
2023-12-29 02:38:39 -0500

    fix: add horext logo SVG file and update VuetifyLogo component

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="VuetifyLogo" alt="Vuetify Logo" src="/horext-logo.svg" />
+  <img class="VuetifyLogo" alt="Vuetify Logo" src="~/assets/images/horext-logo.svg" />
 </template>
 
 <style>
