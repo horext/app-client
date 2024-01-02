46ca3b5f42bfff9773d4475bcf64ba5427d540b2
Franz Bendezu <franzant20@gmail.com>
2024-01-01 23:28:30 -0500

    refactor: import using import map of vuetify

--- modules/vuetify/index.ts
+++ modules/vuetify/index.ts
@@ -5,20 +5,26 @@ import {
   createResolver,
   addImports,
 } from 'nuxt/kit'
-import { COMPONENT_NAMES } from './components'
+import importMap from 'vuetify/dist/json/importMap.json' assert { type: 'json' }
+
 export default defineNuxtModule({
   meta: {
     name: 'vuetify',
   },
   setup() {
     const resolver = createResolver(import.meta.url)
-
-    const components = COMPONENT_NAMES
-    for (const component of components) {
+    const componentsMap: Record<
+      string,
+      {
+        from: string
+        styles: never[]
+      }
+    > = importMap.components
+    for (const component in componentsMap) {
       addComponent({
         name: component,
         export: component,
-        filePath: 'vuetify/components',
+        filePath: 'vuetify/' + componentsMap[component].from.replace(/\/index\.mjs$/, ''),
       })
     }
 
