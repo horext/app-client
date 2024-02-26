26ae603b8c79e3e0607d9a580cf0ea40da9b4d2a
Franz Bendezu <franzant20@gmail.com>
2024-02-26 14:45:21 -0500

    feat: add settings store for managing dark mode

---
+++ stores/settings.ts
@@ -0,0 +1,26 @@
+export const useSettingsStore = defineStore('settings', () => {
+  const theme = useTheme()
+  const localStorage = useLocalStorage()
+
+  const darkMode = computed({
+    get(): boolean {
+      return theme.global.name.value === 'dark'
+    },
+
+    set(val: boolean) {
+      theme.global.name.value = val ? 'dark' : 'light'
+    },
+  })
+  onMounted(async () => {
+    darkMode.value = (await localStorage.getItem('darkMode')) || false
+  })
+
+  watch(darkMode, (val) => {
+    localStorage.setItem('darkMode', val)
+  })
+
+
+  return {
+    darkMode,
+  }
+})
