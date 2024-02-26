cdb4748cf4fc79b193830d27d360feeed5a39516
Franz Bendezu <franzant20@gmail.com>
2024-02-26 14:59:07 -0500

    refactor: improvee homepage layout and button styling

--- pages/index.vue
+++ pages/index.vue
@@ -5,35 +5,36 @@
     cover
     gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
   >
-    <v-container 
-    class="fill-height"
-    >
+    <v-container class="fill-height">
       <v-row align="center" class="text-white mx-auto" justify="center">
-        <v-col class="text-white text-center" cols="12" tag="h1">
-          <span
-            :class="[$vuetify.display.smAndDown ? 'display-1' : 'display-2']"
-            class="font-weight-light"
-          >
-            Bienvenido a
-          </span>
-          <br />
-          <span
-            :class="[$vuetify.display.smAndDown ? 'display-3' : 'display-4']"
-            class="font-weight-black"
-          >
-            Horext
-          </span>
+        <v-col class="text-white text-center" cols="12">
+          <h1>
+            <span
+              :class="[$vuetify.display.smAndDown ? 'display-1' : 'display-2']"
+              class="font-weight-light"
+            >
+              Bienvenido a
+            </span>
+            <br />
+            <span
+              :class="[$vuetify.display.smAndDown ? 'display-3' : 'display-4']"
+              class="font-weight-black"
+            >
+              Horext
+            </span>
+          </h1>
         </v-col>
-
-        <v-btn
-          class="align-self-end"
-          variant="outlined"
-          theme="dark"
-          to="/generator"
-        >
-          <v-icon>mdi-chevron-double-right</v-icon>
-          Generar mis horarios
-        </v-btn>
+        <v-col class="text-center" cols="12">
+          <v-btn
+            class="align-self-end"
+            variant="outlined"
+            theme="dark"
+            to="/generator"
+          >
+            <v-icon start>mdi-chevron-double-right</v-icon>
+            Generar mis horarios
+          </v-btn>
+        </v-col>
       </v-row>
     </v-container>
   </v-img>
