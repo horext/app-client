25a60e8851bc982380e76cb0dd7bb0955bfeddba
Franz Bendezu <franzant20@gmail.com>
2024-02-26 08:26:45 -0500

    refactor: improvee typing for getSchedules function signature

--- utils/core.ts
+++ utils/core.ts
@@ -1,4 +1,5 @@
 import { DateTime } from 'luxon'
+import type { IOccurrence } from '~/interfaces/ocurrences'
 import type { ISchedule } from '~/interfaces/schedule'
 import type { ISelectedSubject, ISubjectSchedule } from '~/interfaces/subject'
 import Event from '~/model/Event'
@@ -40,8 +41,8 @@ export function getSchedules(
     crossEvent: true,
     crossPractices: false,
   },
-): { occurrences: any[]; schedules: any[]; combinations: ISchedule[] } {
-  const occurrences = []
+): { occurrences: IOccurrence[]; schedules: ISubjectSchedule[]; combinations: ISchedule[] } {
+  const occurrences:IOccurrence[] = []
   const maxQuantity = subjects.length
   const indexSchedules = Array(maxQuantity).fill(0)
   const schedules: Array<ISchedule> = []
@@ -60,7 +61,7 @@ export function getSchedules(
   }, 1)
   const crossings = Array(combinations).fill(0)
   for (let i = combinations; i--; ) {
-    const combination: Array<any> = []
+    const combination: Array<ISubjectSchedule> = []
     for (let j = 0; j < indexSchedules.length; j++) {
       const subject = subjects[j]
       const schedule = subjects[j].schedules[indexSchedules[j]]
@@ -80,16 +81,17 @@ export function getSchedules(
       const schedule = currentSchedule.splice(0, 1)
       const events = schedule[0].events
       for (const event of events) {
-        const otherEvents = currentSchedule.map((c: any) => c.events).flat()
+        const otherEvents = currentSchedule.map((c) => c.events).flat()
         otherEvents.push(...myEvents)
         let intersections = 0
         for (const item of otherEvents) {
           if (isIntersects(event, item)) {
-            occurrences.push({
+            const occurrence:IOccurrence = {
               type: 'Cruce de ' + event.title + ' - ' + item.title,
               elementA: event,
               elementB: item,
-            })
+            }
+            occurrences.push(occurrence)
             // if have available crossings
             if (
               crossingCombination + intersections <=
