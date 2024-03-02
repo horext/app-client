9606d151820484f5045b9e2ee9e4221a27b5e527
Franz Bendezu <franzant20@gmail.com>
2024-03-02 18:56:54 -0500

    feat: add IEvent interface with additional properties

--- interfaces/event.ts
+++ interfaces/event.ts
@@ -1,9 +1,12 @@
 export interface IEvent {
   id?: string
   title: string
-  day?: number
+  day: number
+  description?: string
+  location?: string
   color: string
+  category?: string
   type: string
-  startTime?: string
-  endTime?: string
+  startTime: string
+  endTime: string
 }
