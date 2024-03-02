2e3926d9e7cadca05b5477b8805a37e95aa7b36b
Franz Bendezu <franzant20@gmail.com>
2024-03-02 17:05:22 -0500

    refactor: change repository classes to extend BaseRepository

--- repositories/HourlyLoadRepository.ts
+++ repositories/HourlyLoadRepository.ts
@@ -1,10 +1,8 @@
-import type { $Fetch } from 'nitropack'
 import type { IHourlyLoad } from '~/interfaces/houly-load'
+import { BaseRepository } from './BaseRepository'
 
 const PATH_SUBJECTS = 'hourlyLoads'
-export class HourlyLoadRepository {
-  constructor(private $fetch: $Fetch) {}
-
+export class HourlyLoadRepository extends BaseRepository {
   getLatestByFaculty(facultyId: any) {
     return this.$fetch<IHourlyLoad>(PATH_SUBJECTS + '/latest', {
       method: 'GET',
