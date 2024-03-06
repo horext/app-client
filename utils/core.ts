a871dcf96d5352ead8e598d6c1d6223f50405a7e
Franz Bendezu <franzant20@gmail.com>
2024-03-05 19:14:09 -0500

    refactor: change scheduleToEvent function and import statements

--- utils/core.ts
+++ utils/core.ts
@@ -133,28 +133,3 @@ export function getSchedules(
     occurrences,
   }
 }
-
-function scheduleToEvent(
-  schedule: ISubjectSchedule,
-  color: string,
-): Array<Event> {
-  const events: Array<Event> = []
-  const sessions = schedule?.sessions || []
-  for (let i = 0; i < sessions.length; i++) {
-    const course = schedule.subject.course
-    const section = schedule.section.id
-    const event = new Event(
-      sessions[i].day,
-      sessions[i].startTime,
-      sessions[i].endTime,
-      course.id + ' ' + section + ' - ' + course.name,
-      ` Docente: ${sessions[i]?.teacher?.fullName}\n Curso: ${course.id} - ${course.name}\n Sección: ${section}`,
-      sessions[i]?.classroom?.code,
-      color,
-      sessions[i].type.code,
-      'COURSE',
-    )
-    events.push(event)
-  }
-  return events
-}
