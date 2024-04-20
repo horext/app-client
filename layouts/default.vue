08b78b2c60c454e76d03a26898f5fe92156eb563
Franz Bendezu <franzant20@gmail.com>
2024-04-20 10:56:48 -0500

    refactor: migrate default.vue layout component to use script setup syntax

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,13 @@
 <template>
-    <!--    <home-system-bar />-->
-    <home-app-bar />
-    <home-view>
-      <slot />
-    </home-view>
-    <home-footer />
+  <!--    <home-system-bar />-->
+  <home-app-bar />
+  <home-view>
+    <slot />
+  </home-view>
+  <home-footer />
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import HomeAppBar from '~/components/home/AppBar.vue'
 import HomeFooter from '~/components/home/Footer.vue'
 import HomeView from '~/components/home/View.vue'
-
-export default defineComponent({
-  components: {
-    HomeAppBar,
-    HomeFooter,
-    HomeView,
-  },
-  data() {
-    return {
-      listenersStarted: false,
-      permissionGranted: false,
-      idToken: '',
-      loading: false,
-    }
-  },
-})
 </script>
