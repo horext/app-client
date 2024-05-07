5808c4d7d10e8a1d0ceb7b31c2d9435676b0fca2
Franz Bendezu <franzant20@gmail.com>
2024-05-07 09:39:53 -0500

    chore: update useStorage mock in tests/setup/mocks.ts

--- tests/setup/mocks.ts
+++ tests/setup/mocks.ts
@@ -1,8 +1,7 @@
-import { vi } from 'vitest'
 import { mockNuxtImport } from '@nuxt/test-utils/runtime'
 
-mockNuxtImport<typeof useTheme>('useTheme', () =>
-  vi.fn(() => {
-    return 'mocked in setup'
-  }),
-)
+mockNuxtImport<typeof useStorage>('useStorage', () => {
+  return () => {
+    return { value: 'mocked storage' }
+  }
+})
