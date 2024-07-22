d92d674a9132d9ef71f09ab2a21df8d1d319725c
Franz Bendezu <franzant20@gmail.com>
2024-07-22 15:21:11 -0500

    refactor: update getSectionColor utility function to use a predefined set of colors

--- utils/color.ts
+++ utils/color.ts
@@ -1,13 +1,4 @@
 export const getSectionColor = (id: string) => {
-    const colors = [
-      'primary',
-      'secondary',
-      'accent',
-      'error',
-      'info',
-      'success',
-      'warning',
-    ]
-  
-    return colors[id.length % colors.length]
-  }
+  const colors = ['blue', 'purple', 'orange', 'indigo', 'teal']
+  return colors[id.charCodeAt(0) % colors.length]
+}
