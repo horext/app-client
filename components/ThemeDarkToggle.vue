c7969630807f1711fddd81b167df2c0d6c051fd8
Franz Bendezu <franzant20@gmail.com>
2024-07-23 03:47:36 -0500

    refactor: optimize ThemeDarkToggle component for better performance

--- components/ThemeDarkToggle.vue
+++ components/ThemeDarkToggle.vue
@@ -13,53 +13,43 @@
   </v-tooltip>
 </template>
 
-<script lang="ts">
-import { onMounted, ref, watch, defineComponent } from 'vue'
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
 import Animation from '~/assets/lottie/71569-hamster-toggle.json'
 
-export default defineComponent({
-  setup() {
-    const settingsStore = useSettingsStore()
-    const darkModeEl = ref<HTMLElement | null>(null)
-    const darkModeAnimation = useLottie(darkModeEl, {
-      renderer: 'svg',
-      loop: false,
-      autoplay: false,
-      animationData: Animation,
-    })
-    onMounted(() => {
-      darkModeAnimation.value?.setSpeed(2)
-      setAnimationDirection(settingsStore.darkMode)
-    })
-
-    onUnmounted(() => {
-      darkModeAnimation.value?.destroy()
-    })
-
-    const { darkMode } = storeToRefs(settingsStore)
-
-    const toggleDark = () => {
-      darkMode.value = !darkMode.value
-    }
-
-    const setAnimationDirection = (darkMode: boolean) => {
-      if (darkMode) {
-        darkModeAnimation.value!.setDirection(1)
-      } else {
-        darkModeAnimation.value!.setDirection(-1)
-      }
-      darkModeAnimation.value!.play()
-    }
-
-    watch(darkMode, setAnimationDirection)
-    return {
-      toggleDark,
-      darkModeEl,
-      darkModeAnimation,
-      darkMode,
-    }
-  },
+const settingsStore = useSettingsStore()
+const darkModeEl = ref<HTMLElement | null>(null)
+const darkModeAnimation = useLottie(darkModeEl, {
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  animationData: Animation,
 })
+onMounted(() => {
+  darkModeAnimation.value?.setSpeed(2)
+  setAnimationDirection(settingsStore.darkMode)
+})
+
+onUnmounted(() => {
+  darkModeAnimation.value?.destroy()
+})
+
+const { darkMode } = storeToRefs(settingsStore)
+
+const toggleDark = () => {
+  darkMode.value = !darkMode.value
+}
+
+const setAnimationDirection = (darkMode: boolean) => {
+  if (darkMode) {
+    darkModeAnimation.value!.setDirection(1)
+  } else {
+    darkModeAnimation.value!.setDirection(-1)
+  }
+  darkModeAnimation.value!.play()
+}
+
+watch(darkMode, setAnimationDirection)
 </script>
 <style>
 .dark-toggle {
