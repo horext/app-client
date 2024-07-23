c8131b2f5204da2f970cfad8aa24188c622f2381
Franz Bendezu <franzant20@gmail.com>
2024-07-22 19:59:27 -0500

    refactor: optimize Lottie composable for better performance and flexibility

--- composables/lottie.ts
+++ composables/lottie.ts
@@ -4,18 +4,28 @@ import Lottie, {
   type AnimationItem,
   type RendererType,
 } from 'lottie-web'
+import type { Ref } from 'vue'
+import type { ComponentPublicInstance } from 'vue'
+
+export type LottieRef = Ref<ComponentPublicInstance | Element | null>
+
+export type LottieOptions<T extends RendererType = 'svg'> =
+  | Omit<AnimationConfigWithData<T>, 'container'>
+  | Omit<AnimationConfigWithPath<T>, 'container'>
+ 
+export const getElReference = (reference: LottieRef) => {
+  const _reference = reference.value
+  return _reference && '$el' in _reference  ? _reference.$el : _reference
+}
 
 export const useLottie = <T extends RendererType = 'svg'>(
-  reference: Ref<ComponentPublicInstance | Element | null>,
-  options:
-    | Omit<AnimationConfigWithData<T>, 'container'>
-    | Omit<AnimationConfigWithPath<T>, 'container'>,
+  reference: LottieRef,
+  options: LottieOptions<T>,
 ) => {
   const lottie = ref<AnimationItem | null>(null)
   onMounted(() => {
-    const _reference = reference.value
-    if (!_reference) return
-    const el = '$el' in _reference ? _reference.$el : _reference
+    const el = getElReference(reference)
+    if (!el) return
     lottie.value = Lottie.loadAnimation<T>({
       ...options,
       container: el,
