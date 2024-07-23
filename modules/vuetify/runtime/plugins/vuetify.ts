11a6449c7ad86a1acac0a34545d7cfe9b173e2be
Franz Bendezu <franzant20@gmail.com>
2024-07-22 21:46:20 -0500

    refactor: remove unused vuetify color imports

--- modules/vuetify/runtime/plugins/vuetify.ts
+++ modules/vuetify/runtime/plugins/vuetify.ts
@@ -1,7 +1,6 @@
 //@ts-ignore
 import { createVuetify } from 'vuetify/framework'
 import { es } from 'vuetify/locale'
-import colors from 'vuetify/util/colors'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -17,32 +16,7 @@ export default defineNuxtPlugin((nuxtApp) => {
       },
     },
     theme: {
-      themes: {
-        dark: {
-          dark: true,
-          colors: {
-            primary: colors.blue.darken2,
-            accent: colors.grey.darken3,
-            secondary: colors.amber.darken3,
-            info: colors.teal.lighten1,
-            warning: colors.amber.base,
-            error: colors.deepOrange.accent4,
-            success: colors.green.accent3,
-          },
-        },
-        light: {
-          dark: false,
-          colors: {
-            primary: colors.blue.darken2,
-            accent: colors.grey.darken3,
-            secondary: colors.amber.darken3,
-            info: colors.teal.lighten1,
-            warning: colors.amber.base,
-            error: colors.deepOrange.accent4,
-            success: colors.green.accent3,
-          },
-        },
-      },
+     
     },
     icons: {
       defaultSet: 'mdi',
