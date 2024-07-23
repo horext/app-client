20ef1d37d54a603316c226fa2d2703de0cad126e
Franz Bendezu <franzant20@gmail.com>
2024-07-23 01:42:30 -0500

    refactor: replace direct dialog component usage with ConfirmDialog in events.vue

--- pages/generator/events.vue
+++ pages/generator/events.vue
@@ -24,31 +24,13 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5">
-                  ¿Está seguro de eliminar esta actividad?
-                </v-card-title>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn
-                    color="blue-darken-1"
-                    variant="text"
-                    @click="closeDelete"
-                  >
-                    Cancelar
-                  </v-btn>
-                  <v-btn
-                    color="blue-darken-1"
-                    variant="text"
-                    @click="deleteItemConfirm"
-                  >
-                    Aceptar
-                  </v-btn>
-                  <v-spacer />
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <base-confirm-dialog
+              v-model="dialogDelete"
+              @click:confirm="deleteItemConfirm"
+              @click:reject="closeDelete"
+            >
+              > ¿Está seguro de eliminar esta actividad?
+            </base-confirm-dialog>
           </v-toolbar>
           <v-card flat>
             <v-card-text>
