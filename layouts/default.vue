e7802c868e9a8bfd57096fcadb1041a7d4f7d41e
Franz Bendezu <franzant20@gmail.com>
2024-07-23 21:52:34 -0500

    refactor: remove commented code and unused imports in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,8 @@
 <template>
-  <!--    <home-system-bar />-->
   <home-app-bar />
   <home-view>
     <slot />
   </home-view>
   <home-footer />
 </template>
-<script setup lang="ts">
-import HomeAppBar from '~/components/home/AppBar.vue'
-import HomeFooter from '~/components/home/Footer.vue'
-import HomeView from '~/components/home/View.vue'
-</script>
+
