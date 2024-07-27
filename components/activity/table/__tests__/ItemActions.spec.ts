9800e10002a0d6c35e5c412bdf463b117fc5a49e
Franz Bendezu <franzant20@gmail.com>
2024-07-27 08:29:42 -0500

    refactor: add aria-label to ItemActions buttons for accessibility

--- components/activity/table/__tests__/ItemActions.spec.ts
+++ components/activity/table/__tests__/ItemActions.spec.ts
@@ -1,18 +1,39 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import ItemActions from '../ItemActions.vue'
 import { describe, it, expect } from 'vitest'
 import { VBtn } from 'vuetify/components/VBtn'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
 
 describe('ItemActions', () => {
   it('emits click:edit event when edit button is clicked', () => {
-    const wrapper = shallowMount(ItemActions)
-    wrapper.findAllComponents(VBtn).at(0).trigger('click')
+    const wrapper = mount(ItemActions, {
+      global: {
+        plugins: [vuetify],
+      },
+    })
+    const actionButtons = wrapper.findAllComponents(VBtn)
+    const editButton = actionButtons.find(
+      (button) => button.text() === 'Editar Actividad',
+    )
+    expect(editButton).toBeTruthy()
+    editButton?.trigger('click')
     expect(wrapper.emitted('click:edit')).toBeTruthy()
   })
 
   it('emits click:delete event when delete button is clicked', () => {
-    const wrapper = shallowMount(ItemActions)
-    wrapper.findAllComponents(VBtn).at(1).trigger('click')
+    const wrapper = mount(ItemActions, {
+      global: {
+        plugins: [vuetify],
+      },
+    })
+    const actionButtons = wrapper.findAllComponents(VBtn)
+    const deleteButton = actionButtons.find(
+      (button) => button.text() === 'Eliminar Actividad',
+    )
+    expect(deleteButton).toBeTruthy()
+    deleteButton?.trigger('click')
     expect(wrapper.emitted('click:delete')).toBeTruthy()
   })
 })
