8ca7a4b7c3276e5bad20766c62aa87e523db3fd8
Franz Bendezu <franzant20@gmail.com>
2024-08-01 11:00:40 -0500

    refactor: update import paths for ScheduleFavoriteAdd component

--- pages/generator/index.vue
+++ pages/generator/index.vue
@@ -59,7 +59,7 @@
 import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import SchedulesPresentation from '~/components/SchedulesPresentation.vue'
-import ScheduleFavoriteAdd from '~/components/schedule/FavoriteAdd.vue'
+import ScheduleFavoriteAdd from '~/components/schedule/FavoriteToggle.vue'
 import OccurrencesList from '~/components/schedule/OccurrencesList.vue'
 import { useUserConfigStore } from '~/stores/user-config'
 import { useUserEventsStore } from '~/stores/user-events'
