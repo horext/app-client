58e7f4cbe7c1c046c9909610d1746c7486cc6d7e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-18 23:59:20 +0000

    refactor: move to runtime the APIs module

--- modules/apis/index.ts
+++ modules/apis/index.ts
@@ -1,16 +1,5 @@
 import { defineNuxtModule } from 'nuxt/kit'
-import { APIS_REGISTRY } from './registry'
 
-export const createApis = () => {
-  const fetch = $fetch.create({
-    baseURL: '/api',
-  })
-
-  for (const item of APIS_REGISTRY) {
-    const instance = new item.use(fetch)
-    provide(item.provide, instance)
-  }
-}
 
 export default defineNuxtModule({
   meta: {
