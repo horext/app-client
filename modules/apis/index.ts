1e380279dbef4c5080c5dd8870d22006c60f4edc
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 00:55:21 +0000

    refactor: move API instantiation to runtime module and provide fetch injection via plugin

--- modules/apis/index.ts
+++ modules/apis/index.ts
@@ -1,8 +1,14 @@
-import { defineNuxtModule } from 'nuxt/kit'
-
+import { addPlugin, createResolver, defineNuxtModule } from 'nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: 'Apis',
+  },
+  setup() {
+    const resolver = createResolver(import.meta.url)
+
+    addPlugin({
+      src: resolver.resolve('runtime/plugins/fetch-api.ts'),
+    })
   },
 })
