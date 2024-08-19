6ad597ea8bf2a4cc007c78bd361ce9764e3c78a1
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 00:55:35 +0000

    refactor: Move API instantiation to runtime module and provide fetch injection in plans

--- pages/plans.vue
+++ pages/plans.vue
@@ -2,6 +2,12 @@
 import type { IStudyPlan } from '~/interfaces/subject'
 import { useStudyPlanApi } from '~/modules/apis/runtime/composables';
 
+definePageMeta({
+  title: 'Study Plans',
+  description: 'List of study plans',
+  layout: 'plans',
+})
+
 const api = useStudyPlanApi();
 const { data } = useAsyncData<IStudyPlan[]>(() => api.getAll(), {
   default: () => [],
