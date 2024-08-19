d5799da44feed101f71e796d6135b6420ea02df9
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 01:54:06 +0000

    refactor: remove deprecated API plugin and related typings

--- repositories/BaseRepository.ts
+++
@@ -1,5 +0,0 @@
-import type { $Fetch } from 'nitropack'
-
-export abstract class BaseRepository {
-  constructor(protected $fetch: $Fetch) {}
-}
--- repositories/FacultyRepository.ts
+++
@@ -1,9 +0,0 @@
-import type { IOrganization } from '~/interfaces/organization'
-import { BaseRepository } from './BaseRepository'
-
-const PATH_SUBJECTS = 'faculties'
-export class FacultyRepository extends BaseRepository {
-  getAll() {
-    return this.$fetch<IOrganization[]>(PATH_SUBJECTS)
-  }
-}
