9a80bacefb30d90c4a48ef98b9e3cd8ebb72ca93
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 00:55:05 +0000

    refactor: move API instantiation to runtime module and provide fetch injection

--- modules/apis/runtime/index.ts
+++ modules/apis/runtime/index.ts
@@ -1,12 +1,37 @@
+import type { Fetch } from 'ofetch'
+import type { ApiFactory } from './interfaces/registry'
 import { APIS_REGISTRY } from './registry'
+import type { BaseApi } from './resources/base'
+import { provide, inject } from 'vue'
+import type { InjectionKey } from 'vue'
 
-export const createApis = () => {
+export const FETCH_KEY: InjectionKey<Fetch> = Symbol('Fetch')
+
+export const createFetchApi = () => {
   const fetch = $fetch.create({
     baseURL: '/api',
   })
+  return fetch
+}
 
+export const provideFetch = () => {
+  const fetch = createFetchApi()
+  provide(FETCH_KEY, fetch)
+}
+
+export const createApis = () => {
   for (const item of APIS_REGISTRY) {
-    const instance = new item.use(fetch)
-    provide(item.provide, instance)
+    provideApi(item.provide, item.use)
   }
+}
+
+export const provideApi = <T extends BaseApi>(
+  key: InjectionKey<T>,
+  use: ApiFactory<T>,
+) => {
+  const fetch = inject(FETCH_KEY)
+  if (!fetch) {
+    throw new Error('Fetch not provided')
+  }
+  provide(key, new use(fetch))
 }
