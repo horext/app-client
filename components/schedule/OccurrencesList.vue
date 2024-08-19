d2c1d690cba75577d26c1b72766f0da584f91a77
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 21:58:55 +0000

    refactor: migrate OccurrencesList.vue to use Composition API and TypeScript setup syntax

--- components/schedule/OccurrencesList.vue
+++ components/schedule/OccurrencesList.vue
@@ -9,70 +9,61 @@
     <template #[`item.eventTarget.day`]="{ item }">
       <div>
         {{ item.eventTarget.type }}
-        {{ weekDays[item.eventTarget.day] }}
+        {{ WEEK_DAYS_NAMES[item.eventTarget.day] }}
         {{ item.eventTarget.startTime }} - {{ item.eventTarget.endTime }}
       </div>
     </template>
     <template #[`item.eventSource.day`]="{ item }">
       <div>
         {{ item.eventSource.type }}
-        {{ weekDays[item.eventSource.day] }}
+        {{ WEEK_DAYS_NAMES[item.eventSource.day] }}
         {{ item.eventSource.startTime }} - {{ item.eventSource.endTime }}
       </div>
     </template>
   </v-data-table>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import { useVModel } from '@vueuse/core'
 import { WEEK_DAYS_NAMES } from '~/constants/weekdays'
 import type { IIntersectionOccurrence } from '~/interfaces/ocurrences'
 
-export default defineComponent({
-  props: {
-    items: {
-      type: Array as PropType<IIntersectionOccurrence[]>,
-      required: true,
-    },
+const props = defineProps<{
+  items: IIntersectionOccurrence[]
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:items', value: IIntersectionOccurrence[]): void
+}>()
+
+const itemsSync = useVModel(props, 'items', emit)
+const headers = [
+  {
+    title: 'Tipo de Incidencia',
+    align: 'start',
+    value: 'type',
   },
-  emits: ['update:items'],
-  setup(props, { emit }) {
-    const itemsSync = useVModel(props, 'items', emit)
-    const headers = [
-      {
-        title: 'Tipo de Incidencia',
-        align: 'start',
-        value: 'type',
-      },
-      {
-        title: 'Evento A',
-        value: 'eventTarget.title',
-        groupable: false,
-      },
-      {
-        title: 'Dia A',
-        value: 'eventTarget.day',
-        groupable: false,
-      },
-      {
-        title: 'Evento B',
-        value: 'eventSource.title',
-        groupable: false,
-      },
-      {
-        title: 'Dia',
-        value: 'eventSource.day',
-        groupable: false,
-      },
-    ] as const
-    return {
-      weekDays: WEEK_DAYS_NAMES,
-      headers,
-      itemsSync,
-    }
+  {
+    title: 'Evento A',
+    value: 'eventTarget.title',
+    groupable: false,
   },
-})
+  {
+    title: 'Dia A',
+    value: 'eventTarget.day',
+    groupable: false,
+  },
+  {
+    title: 'Evento B',
+    value: 'eventSource.title',
+    groupable: false,
+  },
+  {
+    title: 'Dia',
+    value: 'eventSource.day',
+    groupable: false,
+  },
+] as const
 </script>
 
 <style scoped></style>
