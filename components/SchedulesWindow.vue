06edf1448f2e6fb2bcd6da2840ef035c53ef795c
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-19 21:50:01 +0000

    refactor: migrate SchedulesWindow.vue to use Composition API and TypeScript setup syntax

--- components/SchedulesWindow.vue
+++ components/SchedulesWindow.vue
@@ -26,106 +26,74 @@
   </v-window>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed, ref, watch, type WatchCallback } from 'vue'
+<script setup lang="ts">
+import { computed, ref, watch, type WatchCallback } from 'vue'
 import { useVModel } from '@vueuse/core'
 import ViewList from './schedule/SubjectsTable.vue'
 import ScheduleViewer from '~/components/schedule/Calendar.vue'
 import { ViewMode } from '~/models/ViewMode'
 import type { IScheduleGenerate } from '~/interfaces/schedule'
 import { mdiArrowRightBoldCircle, mdiArrowLeftBoldCircle } from '@mdi/js'
-import { DEFAULT_CALENDAR_WEEK_DAYS } from '~/constants/weekdays'
 
-export default defineComponent({
-  name: 'SchedulesList',
+const props = defineProps<{
+  schedules: IScheduleGenerate[]
+  weekDays: number[]
+  currentSchedule: IScheduleGenerate | undefined
+  mode: ViewMode
+}>()
 
-  components: {
-    ScheduleViewer,
-    ViewList,
+const emit = defineEmits<{
+  (event: 'update:currentSchedule', value: IScheduleGenerate): void
+}>()
+
+const index = ref(0)
+
+const syncedCurrentSchedule = useVModel(props, 'currentSchedule', emit)
+
+const MODES = ViewMode
+
+const onChangeSchedules: WatchCallback = (newValue, oldValue) => {
+  if (oldValue.length !== newValue.length) {
+    index.value = 0
+  }
+}
+watch(() => props.schedules, onChangeSchedules)
+
+const onChangeSchedule: WatchCallback = (value) => {
+  if (index.value >= props.schedules.length) {
+    index.value = props.schedules.length - 1
+  }
+  syncedCurrentSchedule.value = value
+}
+
+const schedule = computed(() => props.schedules[index.value])
+
+watch(schedule, onChangeSchedule, { immediate: true })
+
+const page = computed<number>({
+  get() {
+    return index.value + 1
   },
-
-  props: {
-    schedules: {
-      type: Array as PropType<IScheduleGenerate[]>,
-      default: () => [],
-    },
-    weekDays: {
-      type: Array as PropType<number[]>,
-      default: () => DEFAULT_CALENDAR_WEEK_DAYS,
-    },
-    currentSchedule: {
-      type: Object as PropType<IScheduleGenerate | undefined>,
-      default: () => undefined,
-    },
-    mode: {
-      type: String,
-      default: () => ViewMode.CALENDAR,
-    },
-  },
-  emits: ['update:currentSchedule'],
-  setup(props, { emit }) {
-    const index = ref(0)
-
-    const syncedCurrentSchedule = useVModel(props, 'currentSchedule', emit)
-
-    const MODES = ViewMode
-
-    const onChangeSchedules: WatchCallback = (newValue, oldValue) => {
-      if (oldValue.length !== newValue.length) {
-        index.value = 0
-      }
-    }
-    watch(() => props.schedules, onChangeSchedules)
-
-    const onChangeSchedule: WatchCallback = (value) => {
-      if (index.value >= props.schedules.length) {
-        index.value = props.schedules.length - 1
-      }
-      syncedCurrentSchedule.value = value
-    }
-
-    const shareDialog = ref(false)
-    const schedule = computed(() => props.schedules[index.value])
-
-    watch(schedule, onChangeSchedule, { immediate: true })
-
-    const page = computed<number>({
-      get() {
-        return index.value + 1
-      },
-      set(value) {
-        index.value = value - 1
-      },
-    })
-
-    const next = () => {
-      if (page.value < props.schedules.length) {
-        page.value = page.value + 1
-      } else {
-        page.value = 1
-      }
-    }
-
-    const prev = () => {
-      if (page.value > 1) {
-        page.value = page.value - 1
-      } else {
-        page.value = props.schedules.length
-      }
-    }
-
-    return {
-      schedule,
-      page,
-      next,
-      prev,
-      MODES,
-      shareDialog,
-      mdiArrowRightBoldCircle,
-      mdiArrowLeftBoldCircle,
-    }
+  set(value) {
+    index.value = value - 1
   },
 })
+
+const next = () => {
+  if (page.value < props.schedules.length) {
+    page.value = page.value + 1
+  } else {
+    page.value = 1
+  }
+}
+
+const prev = () => {
+  if (page.value > 1) {
+    page.value = page.value - 1
+  } else {
+    page.value = props.schedules.length
+  }
+}
 </script>
 
 <style lang="sass">
