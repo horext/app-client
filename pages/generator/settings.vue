b5dd4339206664046da57c5532d1780702b4e209
Franz Bendezu <franzant20@gmail.com>
2024-08-21 12:24:50 -0500

    refactor: update settings.vue to include calendar configuration and save functionality

--- pages/generator/settings.vue
+++ pages/generator/settings.vue
@@ -1,18 +1,45 @@
 <template>
   <v-container>
-    <v-card>
-      <SettingInitial />
-    </v-card>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <SettingInitial />
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title> Configuración de calendario </v-card-title>
+          <v-card-subtitle>
+            Seleccione los días de la semana que desea mostrar en el calendario
+          </v-card-subtitle>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12">
+                <v-checkbox
+                  v-for="day in WEEK_DAYS_NAMES"
+                  :key="day"
+                  :label="day"
+                  multiple
+                />
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn variant="text" @click="save">Guardar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import SettingInitial from '~/components/setting/Initial.vue'
-export default defineComponent({
-  name: 'Advance',
-  components: {
-    SettingInitial,
-  },
-})
+import { useUserConfigStore } from '~/stores/user-config'
+import { WEEK_DAYS_NAMES } from '~/constants/weekdays'
+
+const store = useUserConfigStore()
+
+const save = () => {}
 </script>
