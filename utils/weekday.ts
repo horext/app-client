b1813c83e1fb8b1a84251072336f441091f14d46
Franz Bendezu <franzant20@gmail.com>
2024-08-21 12:02:23 -0500

    refactor: add getWeekdayName and getWeekdayISONames functions to weekday.ts

--- utils/weekday.ts
+++ utils/weekday.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
+import { WEEK_DAYS_NAMES } from '~/constants/weekdays'
 import type { Weekdays } from '~/interfaces/event'
+import type { WeekdaysISO } from '~/modules/h-calendar/runtime/constants/week'
 
 export const weekdayToDatetime = (weekday: Weekdays, time: string) => {
   const date = DateTime.fromISO(time).set({
@@ -15,4 +17,11 @@ export const weekdayToDate = (weekday: Weekdays) => {
 
 export const convertToDate = (weekday: Weekdays, startTime: string) => {
   return weekdayToDatetime(weekday, startTime)
+}
+
+export const getWeekdayName = (weekday: Weekdays) => {
+  return WEEK_DAYS_NAMES[weekday]
+}
+export const getWeekdayISONames = (weekday: WeekdaysISO) => {
+  return WEEK_DAYS_NAMES[weekday % 7]
 }
