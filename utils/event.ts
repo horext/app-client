7a56448b270d7124b9365c127579f97b787d032e
Franz Bendezu <franzant20@gmail.com>
2024-08-22 10:59:01 -0500

    refactor: set  scheduleToEvent function to use ISubjectScheduleGenerate interface

--- utils/event.ts
+++ utils/event.ts
@@ -1,5 +1,5 @@
 import type { IInterval } from '~/interfaces/interval'
-import type { ISubjectSchedule } from '~/interfaces/subject'
+import type { ISubjectScheduleGenerate } from '~/interfaces/schedule'
 import Event from '~/models/Event'
 
 export const isIntersects = (
@@ -11,7 +11,7 @@ export const isIntersects = (
   )
 
 export function scheduleToEvent(
-  schedule: ISubjectSchedule,
+  schedule: ISubjectScheduleGenerate,
   color: string,
 ): Array<Event> {
   const events: Array<Event> = []
