0948177efca11108acec86a3017746fd92329dc8
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-02-22 04:49:31 +0000

    fix(types): refine event emitter type definitions in ExportActions component fix(layouts): disable eslint rule for multiple template roots in default layout

--- components/schedule/ExportActions.vue
+++ components/schedule/ExportActions.vue
@@ -38,6 +38,6 @@ defineProps<{
 }>()
 
 defineEmits<{
-  (event: 'download:pdf'| 'download:image', value: MouseEvent): void
+  (event: 'download:pdf' | 'download:image', value: MouseEvent): void
 }>()
 </script>
