41f58db590bb86bfc006b426abb2c8f4c19425f1
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-02-22 06:45:40 +0000

    feat(schedules): implement schedule route validation with Zod schema

--- server/routes/schedules/[scheduleId].delete.ts
+++ server/routes/schedules/[scheduleId].delete.ts
@@ -1,10 +1,13 @@
 import { useScheduleRepository } from '~/server/provider/schedule.repository.provider'
+import { scheduleRouteSchema } from '~/server/schemas/schedule-route.schema'
 import { getAuthenticatedUser } from '~/server/utils/auth'
 
 export default defineEventHandler({
   onRequest: authorizeEventRequest,
   handler: async (event) => {
-    const { scheduleId } = getRouterParams(event)
+    const { scheduleId } = await getValidatedRouterParams(event, (data) =>
+      scheduleRouteSchema.parse(data),
+    )
     const user = getAuthenticatedUser(event)
     const scheduleRepository = await useScheduleRepository(event)
 
--- server/routes/schedules/[scheduleId].patch.ts
+++ server/routes/schedules/[scheduleId].patch.ts
@@ -2,11 +2,14 @@ import { readValidatedBody } from 'h3'
 import { scheduleSchema } from '../../schemas/schedule.schema'
 import { useScheduleRepository } from '~/server/provider/schedule.repository.provider'
 import { getAuthenticatedUser } from '~/server/utils/auth'
+import { scheduleRouteSchema } from '~/server/schemas/schedule-route.schema'
 
 export default defineEventHandler({
   onRequest: authorizeEventRequest,
   handler: async (event) => {
-    const { scheduleId } = getRouterParams(event)
+    const { scheduleId } = await getValidatedRouterParams(event, (data) =>
+      scheduleRouteSchema.parse(data),
+    )
     const user = getAuthenticatedUser(event)
     const body = await readValidatedBody(event, (data) =>
       scheduleSchema.partial().parse(data),
