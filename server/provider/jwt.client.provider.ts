96a5986cf38e5c18c20db7a208eddfe5569a54a4
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-02-22 05:28:04 +0000

    fix(auth): update useJwtClient to return IJwtClient interface for better type safety

--- server/provider/jwt.client.provider.ts
+++ server/provider/jwt.client.provider.ts
@@ -1,11 +1,12 @@
 import type { H3Event, EventHandlerRequest } from 'h3'
 import { useRuntimeConfig } from '#imports'
+import type { IJwtClient} from '../clients/jwt.client';
 import { JwtClient } from '../clients/jwt.client'
 import { JwksClient } from 'jwks-rsa'
 import { createLazySingleton } from './provider'
 
 export const useJwtClient = createLazySingleton(
-  (event: H3Event<EventHandlerRequest>): JwtClient => {
+  (event: H3Event<EventHandlerRequest>): IJwtClient => {
     const { auth } = useRuntimeConfig(event)
     return new JwtClient(
       {
