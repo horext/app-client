06b9b46dd9b3e3688dc341ddcf5ce29c91cbcc09
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-03-19 18:47:36 +0000

    fix: remove unused API handler from server code

--- server/api/[...].ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler((event) => {
-  const { apiUrl: target } = useRuntimeConfig()
-  const apiPath = event.path.replace('/api', '')
-  const targetPath = target + apiPath
-  return proxyRequest(event, targetPath)
-})
