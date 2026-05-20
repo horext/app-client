96ed34386bf70c94af7addb67d5d2e1004982d48
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-05-20 21:03:34 +0000

    fix: wrap ScheduleActionsBar in ClientOnly for better rendering

--- app/components/SchedulesPresentation.vue
+++ app/components/SchedulesPresentation.vue
@@ -11,12 +11,14 @@
     <div class="d-flex align-center justify-center flex-wrap ma-1 ga-1">
       <slot name="subtitle" :item="currentSchedule">
         <slot name="subtitle-items" :item="currentSchedule" />
-        <ScheduleActionsBar
-          v-if="schedules.length > 0 && currentSchedule"
-          :current-schedule="currentSchedule"
-          :mode="mode"
-          :path="path"
-        />
+        <ClientOnly>
+          <ScheduleActionsBar
+            v-if="schedules.length > 0 && currentSchedule"
+            :current-schedule="currentSchedule"
+            :mode="mode"
+            :path="path"
+          />
+        </ClientOnly>
       </slot>
     </div>
 
