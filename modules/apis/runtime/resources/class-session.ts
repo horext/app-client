4e8270bc2612f0b0ae850b5eb47c910aab8eb086
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-05-20 23:07:38 +0000

    refactor: remove redundant findScheduleId method from ClassSessionApi

--- modules/apis/runtime/resources/class-session.ts
+++ modules/apis/runtime/resources/class-session.ts
@@ -4,19 +4,10 @@ import { BaseApi } from './base'
 const PATH_SUBJECTS = 'classSessions'
 
 export interface IClassSessionApi {
-  findScheduleId(schedule: number): Promise<ISession[]>
   findScheduleIds(schedulesIds: Array<number>): Promise<ISession[]>
 }
 
 export class ClassSessionApi extends BaseApi implements IClassSessionApi {
-  findScheduleId(schedule: number) {
-    return this.$fetch<ISession[]>(PATH_SUBJECTS, {
-      method: 'GET',
-      params: {
-        schedule,
-      },
-    })
-  }
 
   findScheduleIds(schedulesIds: Array<number>) {
     return this.$fetch<ISession[]>(PATH_SUBJECTS, {
