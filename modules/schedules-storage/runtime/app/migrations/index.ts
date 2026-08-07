963fdbc63fb547dc58e354a871366a076228fffd
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-07 13:09:07 +0000

    refactor: remove obsolete migration files for local storage events and activity sessions

--- modules/schedules-storage/runtime/app/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Migration } from './types'
-import v4 from './v4_localstorage_schedules_to_indexeddb'
-import v2 from './v2_localstorage_events_to_indexeddb'
-import v3 from './v3_localstorage_config_to_indexeddb'
-import v1 from './v1_organization_to_profile'
-import v5 from './v5_localstorage_subjects_to_indexeddb'
-import v6 from './v6_activity_sessions'
-
-export const migrations: Migration[] = [v1, v2, v3, v4, v5, v6]
