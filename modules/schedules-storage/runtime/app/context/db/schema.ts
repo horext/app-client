8d6f6ba5618573e8c1d42da8fba1e8b81cac90d6
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-12 08:37:02 +0000

    refactor: update HorextDB type to allow union with ReplicableSchemas

--- modules/schedules-storage/runtime/app/context/db/schema.ts
+++ modules/schedules-storage/runtime/app/context/db/schema.ts
@@ -64,7 +64,7 @@ export type ReplicableSchemas = {
   }
 }
 
-export type HorextDB = DBSchema & ReplicableSchemas
+export type HorextDB = DBSchema | ReplicableSchemas
 
 export type ReplicableStore =
   | StoresDB.ACADEMIC_CONFIG
