140d18057c343c12f193905b69c3b6ec5a14ebc0
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-18 03:15:28 +0000

    fix: use global crypto

--- shared/domain/types/ids.ts
+++ shared/domain/types/ids.ts
@@ -1,4 +1,4 @@
-import { randomUUID, type UUID } from 'crypto'
+import type { UUID } from 'crypto'
 
 export declare const brand: unique symbol
 export type Brand<T, TBrand extends string> = T & { readonly [brand]: TBrand }
@@ -6,5 +6,5 @@ export type Brand<T, TBrand extends string> = T & { readonly [brand]: TBrand }
 export type BrandUUID<TBrand extends string> = Brand<UUID, TBrand>
 
 export const makeUUID = <T extends Brand<UUID, string>>(): T => {
-  return randomUUID() as T
+  return crypto.randomUUID() as T
 }
