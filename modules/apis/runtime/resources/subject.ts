2f174a985740c14be0b42060c90114c1b4151fb3
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-19 04:30:08 +0000

    refactor: migrate api resource from course to subject

--- modules/apis/runtime/resources/subject.ts
+++ modules/apis/runtime/resources/subject.ts
@@ -2,7 +2,7 @@ import type { IPageResponse } from '../interfaces/page'
 import type { ISubjectResponse } from '../interfaces/subject'
 import { BaseApi } from './base'
 
-export interface ICourseApi {
+export interface ISubjectApi {
   findBySearch(
     search: string,
     speciality: number,
@@ -12,7 +12,7 @@ export interface ICourseApi {
 
 const PATH_SUBJECTS = 'subjects'
 
-export class CourseApi extends BaseApi implements ICourseApi {
+export class SubjectApi extends BaseApi implements ISubjectApi {
   public findBySearch(search: string, speciality: number, hourlyLoad: number) {
     return this.$fetch<IPageResponse<ISubjectResponse>>(
       PATH_SUBJECTS + '?search=' + search,
