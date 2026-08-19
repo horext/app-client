549e1210d8e4e49429bb9ff0bad2fc94b4de7220
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-19 21:21:23 +0000

    fix: prevent issues for duplicate submits on initial form

--- app/components/setting/Initial.vue
+++ app/components/setting/Initial.vue
@@ -40,7 +40,7 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn type="submit" variant="text" @click="ending"> Guardar </v-btn>
+        <v-btn type="submit" variant="text"> Guardar </v-btn>
       </v-card-actions>
     </v-card>
   </v-form>
@@ -58,6 +58,7 @@ import {
 } from '~~/modules/apis/runtime/composables'
 import type { VForm } from 'vuetify/components/VForm'
 import { storeToRefs } from 'pinia'
+import type { SubmitEventPromise } from 'vuetify'
 
 defineProps<{ loading?: boolean }>()
 const emit = defineEmits<{
@@ -137,9 +138,8 @@ const { data: faculties, pending: loadingFaculties } = useAsyncData<
   default: () => [],
 })
 
-const form = ref<VForm>()
-const ending = async () => {
-  const formValue = await form.value?.validate()
+const ending = async (e: SubmitEventPromise) => {
+  const formValue = await e
   if (!formValue?.valid) return
   emit(
     'submit',
