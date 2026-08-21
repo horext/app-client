a135e7e27fe43238731af99c589db01f4f3bb0a6
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-21 20:41:24 +0000

    refactor: simplify schedule api normalization

--- app/mappers/schedule/api.ts
+++ app/mappers/schedule/api.ts
@@ -13,25 +13,13 @@ function toAppSchedule(
   schedule: IScheduleResponse,
 ): Pick<ISubjectSchedule, 'id' | 'section' | 'sessions'> {
   return {
-    id: schedule.id,
-    section: {
-      id: schedule.section.id,
-    },
+    ...schedule,
     sessions: schedule.sessions.map((session) => ({
-      id: session.id,
-      schedule: {
-        id: session.schedule.id,
-      },
+      ...session,
       classroom: {
-        id: session.classroom.id,
-        code: session.classroom.code,
+        ...session.classroom,
         name: session.classroom.name ?? undefined,
       },
-      teacher: session.teacher,
-      type: session.type,
-      day: session.day,
-      startTime: session.startTime,
-      endTime: session.endTime,
     })),
   }
 }
@@ -40,9 +28,7 @@ export function toAppScheduleSubject(
   scheduleSubject: IScheduleSubjectResponse,
 ): IScheduleSubject {
   return {
-    id: scheduleSubject.id,
-    subject: scheduleSubject.subject,
-    hourlyLoad: scheduleSubject.hourlyLoad,
+    ...scheduleSubject,
     schedule: toAppSchedule(scheduleSubject.schedule),
   }
 }
